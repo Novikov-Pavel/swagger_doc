2b488ad7dfc40a6c1da0c13577ca6957670d57b0
Pavel Novikov <novikov@jobfrontend.ru>
2024-08-09 14:18:14 +0300

    Рефакторинг модалки

---
+++ components/Modal.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="modal">
+    <h2 class="modal__title">Информация по отклику</h2>
+    <textarea
+      v-model="textaria"
+      placeholder="Комментарий"
+      class="modal__textaria"
+    />
+    <div class="modal__inputs">
+      <input
+        v-model="tel"
+        type="tel"
+        placeholder="Телефон"
+        class="modal__input"
+      />
+      <input
+        v-model="email"
+        type="email"
+        placeholder="E-mail*"
+        class="modal__input"
+      />
+      <input
+        v-model="tg"
+        type="text"
+        placeholder="Телеграм*"
+        class="modal__input"
+      />
+    </div>
+    <UCheckbox v-model="checkbox" name="notifications" color="primary">
+      <template #label>
+        <span class="modal__checkbox">
+          Прошу не звонить и написать мне в мессенджер
+        </span>
+      </template>
+    </UCheckbox>
+    <UButton class="task__button modal__close"> Отправить </UButton>
+  </div>
+</template>
+
+<script setup lang="ts">
+const textaria = ref<string>("");
+const tel = ref<string>("");
+const email = ref<string>("");
+const tg = ref<string>("");
+const checkbox = ref<boolean>(false);
+</script>
+
+<style scopedlang="scss">
+.modal {
+  background: #f3f5f8;
+  box-shadow: 0px 16px 36px 0px #0000001f;
+  padding: 36px 28px 36px 28px;
+  border-radius: 12px;
+  &__title {
+    font-size: 20px;
+    font-weight: 700;
+    line-height: 24px;
+  }
+  &__textaria {
+    padding: 10px 12px;
+    margin: 24px 0 16px;
+    background: #ffffff;
+    border-radius: 8px;
+    resize: none;
+    width: 100%;
+    height: 84px;
+  }
+  &__inputs {
+    display: flex;
+    flex-direction: column;
+    gap: 12px;
+    margin-bottom: 16px;
+  }
+  &__input {
+    padding: 10px 12px;
+    border-radius: 8px;
+  }
+  &__checkbox {
+    color: #06162e;
+    font-size: 12px;
+    line-height: 16.8px;
+  }
+  &__close {
+    margin-top: 24px;
+  }
+}
+</style>
--- components/Task.vue
+++ components/Task.vue
@@ -15,44 +15,7 @@
         Взять задачу
       </UButton>
       <UModal v-model="isOpen">
-        <div class="modal">
-          <h2 class="modal__title">Информация по отклику</h2>
-          <textarea
-            v-model="textaria"
-            placeholder="Комментарий"
-            class="modal__textaria"
-          />
-          <div class="modal__inputs">
-            <input
-              v-model="tel"
-              type="tel"
-              placeholder="Телефон"
-              class="modal__input"
-            />
-            <input
-              v-model="email"
-              type="email"
-              placeholder="E-mail*"
-              class="modal__input"
-            />
-            <input
-              v-model="tg"
-              type="text"
-              placeholder="Телеграм*"
-              class="modal__input"
-            />
-          </div>
-          <UCheckbox v-model="checkbox" name="notifications" color="primary">
-            <template #label>
-              <span class="modal__checkbox">
-                Прошу не звонить и написать мне в мессенджер
-              </span>
-            </template>
-          </UCheckbox>
-          <UButton @click="isOpen = false" class="task__button modal__close">
-            Отправить
-          </UButton>
-        </div>
+        <Modal />
       </UModal>
     </div>
   </div>
@@ -60,14 +23,9 @@
 
 <script setup lang="ts">
 const isOpen = ref<boolean>(false);
-const textaria = ref<string>("");
-const tel = ref<string>("");
-const email = ref<string>("");
-const tg = ref<string>("");
-const checkbox = ref<boolean>(false);
 </script>
 
-<style scoped lang="scss">
+<style lang="scss">
 .task {
   background-color: #ffffff;
   border-radius: 12px;
@@ -106,43 +64,6 @@ const checkbox = ref<boolean>(false);
     padding: 13px 24px;
     font-size: 16px;
     line-height: 22.4px;
-  }
-}
-.modal {
-  background: #f3f5f8;
-  box-shadow: 0px 16px 36px 0px #0000001f;
-  padding: 36px 28px 36px 28px;
-  border-radius: 12px;
-  &__title {
-    font-size: 20px;
-    font-weight: 700;
-    line-height: 24px;
-  }
-  &__textaria {
-    padding: 10px 12px;
-    margin: 24px 0 16px;
-    background: #ffffff;
-    border-radius: 8px;
-    resize: none;
-    width: 100%;
-  }
-  &__inputs {
-    display: flex;
-    flex-direction: column;
-    gap: 12px;
-    margin-bottom: 16px;
-  }
-  &__input {
-    padding: 10px 12px;
-    border-radius: 8px;
-  }
-  &__checkbox {
-    color: #06162e;
-    font-size: 12px;
-    line-height: 16.8px;
-  }
-  &__close {
-    margin-top: 24px;
   }
 }
 </style>
