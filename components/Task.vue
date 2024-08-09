ca1e03f6d2547246f8e8f295f592456367bbccd9
Pavel Novikov <novikov@jobfrontend.ru>
2024-08-09 14:33:00 +0300

    отрисовал таски

--- components/Task.vue
+++ components/Task.vue
@@ -1,15 +1,12 @@
 <template>
   <div class="task">
-    <div class="task__type">Дизайн</div>
-    <div class="task__title">Сделать креативы для ВК</div>
-    <div class="task__info">
-      Сделать креативы в ВК, дешево, под арбитражную тематику Портфолио -
-      обязательно
-    </div>
+    <div class="task__type">{{ task.type }}</div>
+    <div class="task__title">{{ task.title }}</div>
+    <div class="task__info">{{ task.info }}</div>
     <div class="task__book">
       <div class="task__price">
-        <p>500 руб.</p>
-        <p>5 дней</p>
+        <p>{{ task.price }}</p>
+        <p>{{ task.deadline }}</p>
       </div>
       <UButton @click="isOpen = true" class="task__button">
         Взять задачу
@@ -22,6 +19,16 @@
 </template>
 
 <script setup lang="ts">
+interface TaskProps {
+  type: string;
+  title: string;
+  info: string;
+  price: string;
+  deadline: string;
+}
+defineProps<{
+  task: TaskProps;
+}>();
 const isOpen = ref<boolean>(false);
 </script>
 
