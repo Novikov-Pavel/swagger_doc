2cece4c77eac34dbf53ce3f517e2dccad1284966
Pavel Novikov <novikov@jobfrontend.ru>
2024-08-09 10:12:12 +0300

    Инициализацяи проекта

---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>layout</div>
+  <slot />
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <default>123</default>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
